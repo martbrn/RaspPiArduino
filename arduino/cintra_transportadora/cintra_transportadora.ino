4b1868da9e3c087a7f2b7918d7107aa46296f4e5
JuanMSK <juan.saenz.k@gmail.com>
2020-09-18 20:34:55 -0300

    agregando el contador de cargas y mesa vibratoria

--- arduino/cintra_transportadora/cintra_transportadora.ino
+++ arduino/cintra_transportadora/cintra_transportadora.ino
@@ -8,18 +8,34 @@ long d; //distancia en centimetros
 int motor = 6;
 int sentido1 = 7;
 int sentido2 = 8;
-int pwm;
 int giro1=HIGH; //declarar un sentido de giro inicial
 int giro2=LOW; //declarar un sentido de giro inicial
 int estado=LOW;
-
+int pwm;
+//vibrador
+int motorV = 3;
+int sentidoV1 = 4;
+int sentidoV2 = 5;
+int giroV1=HIGH; //declarar un sentido de giro inicial
+int giroV2=LOW; //declarar un sentido de giro inicial
+int estadoV=LOW;
+int velPWM=255;
+int contaBarra=0;
 void setup() {
   Serial.begin(9600);
+//motor
   pinMode(motor, OUTPUT);
   pinMode(sentido1,OUTPUT);
   pinMode(sentido2,OUTPUT);
   digitalWrite(sentido1,giro1);
   digitalWrite(sentido2,giro2);
+//motorV
+  pinMode(motorV, OUTPUT);
+  pinMode(sentidoV1,OUTPUT);
+  pinMode(sentidoV2,OUTPUT);
+  digitalWrite(sentidoV1,giroV1);
+  digitalWrite(sentidoV2,giroV2);
+
 //sensor HC-SR04
   pinMode(Trigger, OUTPUT); //pin como salida
   pinMode(Echo, INPUT);  //pin como entrada
@@ -42,9 +58,17 @@ void loop() {
   Serial.print("cm");
   Serial.println();
 
-if (d > 5 && !estado){acelerar(motor);
+//contador
+Serial.print("Cantidad de cargas: ");
+Serial.println(contaBarra);
+
+if (d > 5 && !estado){
+    acelerar(motor);
   }
-  else if (d < 5 && estado) {desacelerar(motor);
+  else if (d < 5 && estado) {
+    desacelerar(motor);
+    contaBarra=contaBarra + 1;
+    vibrar(motorV);
   };
   
   delay(100);   
@@ -54,7 +78,6 @@ if (d > 5 && !estado){acelerar(motor);
 void acelerar(int M){
   for (pwm = 0; pwm < 256; pwm++){
      analogWrite(M,pwm); 
-       Serial.print(pwm); 
      delay(20);
   }
   estado=HIGH;
@@ -62,7 +85,6 @@ void acelerar(int M){
 void desacelerar(int M){
   for (pwm = 255; pwm > 0; pwm--){
      analogWrite(M,pwm); 
-       Serial.print(pwm); 
      delay(5);
   }
   estado=LOW;
@@ -80,6 +102,11 @@ void girarIzquierda(){
   acelerar(motor);
   }
 void detener(int M){
-     analogWrite(M,0); 
-       estado=LOW;
+   analogWrite(M,0); 
+   estado=LOW;
+  }
+void vibrar(int mV){
+  analogWrite(mV,velPWM); 
+  delay(3000);
+  analogWrite(mV,0); 
   }
